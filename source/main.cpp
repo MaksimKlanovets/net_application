1f0150a3e69a584027d8c69d178fb8f9c2848da2
MaksimKlanovets <klanovetsm@gmail.com>
2021-05-20 00:17:40 +0300

    test meson

---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include "../headers/Wrapper.hpp"
+//
+// main is where all program execution starts
+//
+int main() {
+  
+
+
+Wrapper wr;
+wr.sleep(1000);
+
+printf("Hello there.\n");
+
+  return 0;
+}
